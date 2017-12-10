f7756933c5da5437f87df0a50cbfa52d2bf513be
Cian Woods <cianwoods@me.com>
2017-12-10 10:59:08 +0000

    cart table

---
+++ Lab_Database/cartTable.js
@@ -0,0 +1,13 @@
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('database.db');
+const fs = require('fs')
+
+db.serialize(function() {
+    db.run("CREATE TABLE orders(" +
+  ",price VARCHAR(50)" +
+  ",date   VARCHAR(84)" +
+  ")"
+    );
+});
+
+db.close();
